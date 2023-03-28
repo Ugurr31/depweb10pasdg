b34cbb95910718f9a00517d60f01cdff035496d4
Ugurr31 <95451043+Ugurr31@users.noreply.github.com>
2023-03-29 00:06:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+/*
+    THIS FILE WAS DEVELOPED BY MEHMET GUDUK
+    © 2022 COPYRIGHT, LICENSED WITH GPL-3.0 LICENSE, AUTHOR IS MEHMET GUDUK
+    https://github.com/mehmetguduk
+*/
+
+const img = document.querySelectorAll("img");
+img.forEach(element => {
+    element.setAttribute("draggable", "false");
+})
+
+/* DISABLED RIGHT CLICK */
+
+function clickIE() {
+    if (document.all) {
+        return false;
+    }
+}
+function clickNS(e) {
+    if (document.layers || (document.getElementById && !document.all)) {
+        if (e.which == 2 || e.which == 3) {
+            (message);
+            return false;
+        }
+    }
+}
+if (document.layers) {
+    document.captureEvents(Event.MOUSEDOWN);
+    document.onmousedown = clickNS;
+}
+else {
+    document.onmouseup = clickNS;
+    document.oncontextmenu = clickIE;
+}
+document.oncontextmenu = new Function("return false");
