1c1eb3d8d5c93b1d22f3c54df71f412cbfdac375
Ugurr31 <95451043+Ugurr31@users.noreply.github.com>
2023-03-29 00:37:01 +0300

    Update cities.js

--- dropdowns/cities.js
+++ dropdowns/cities.js
@@ -5,7 +5,7 @@
 */
 
 const city_list = [
-    { name: "Türkiye Geneli", population: 83614362, lucky: 5304220, ratio: 15 },
+    { name: "Türkiye Geneli", population: , lucky: 5304220, ratio: 15 },
     { name: "Adana", population: 2258718, lucky: 163817, ratio: 13 },
     { name: "Adıyaman", population: 632459, lucky: 17073, ratio: 37 },
     { name: "Afyonkarahisar", population: 736912, lucky: 16362, ratio: 45 },
